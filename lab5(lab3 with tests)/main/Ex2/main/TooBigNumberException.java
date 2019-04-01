44e4b7a1d412cac9c7d0d751380e21f43d99d4db
jbryzek <48593836+jbryzek@users.noreply.github.com>
2019-04-01 21:58:15 +0200

    Delete TooBigNumberException.java

--- lab5(lab3 with tests)/main/Ex2/main/TooBigNumberException.java
+++
@@ -1,6 +0,0 @@
-package Ex2;
-
-public class TooBigNumberException extends Exception{
-    public TooBigNumberException() {
-        System.out.println("Too big number, it must be under 1 000 000");
-}}
