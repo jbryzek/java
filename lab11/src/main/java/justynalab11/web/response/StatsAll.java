ed9b997db09dbacd991d94a12860960a035a5899
jbryzek <48593836+jbryzek@users.noreply.github.com>
2019-06-03 18:01:19 +0200

    Create StatsAll.java

---
+++ lab11/src/main/java/justynalab11/web/response/StatsAll.java
@@ -0,0 +1,9 @@
+package justyna.lab11.web.response;
+
+public class StatsAll {
+    int[] rankOfCreatedSurveys;
+    int[] rankOfAnswersPerSurvey;
+    double averageAmountOfSurveysPerUser;
+    double averageAmountOfAnswersPerSurvey;
+    int amountOfSurveys;
+}
